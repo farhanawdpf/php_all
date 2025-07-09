601f5e66632673e2aecef47c458422068a88f633
farhanawdpf <farhanawdpf@gmail.com>
2025-07-09 11:24:45 +0600

    upload

--- authentication/auth/login.php
+++ authentication/auth/login.php
@@ -3,13 +3,30 @@
 if(isset($_POST['btnLogin'])){ 
     $username=$_POST['txtUsername'];
     $password=$_POST["txtPassword"];
+     $file=file("data.txt");
 
-    if($username=="admin" && $password==123){ 
+
+foreach($file as $line){
+		  
+		  list($_username,$_password)=explode(",",$line);
+		  
+		  if($_username==$username && $_password==$password){
+			
+			 header("location:demo.php");
+			  
+		  }else{
+			  $msg="Username or Password is incorrect!";
+		  }
+		  
+	  }
+
+
+    // if($username=="admin" && $password==123){ 
   
-        header("location:demo.php");
-    }else{ 
-        $msg="error";
-    }
+    //     header("location:demo.php");
+    // }else{ 
+    //     $msg="error";
+    // }
 
 }
 
