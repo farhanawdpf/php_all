ecf890385385f886c8497c83ef073376bb33a6ae
farhanawdpf <farhanawdpf@gmail.com>
2025-07-12 13:07:19 +0600

    update

--- file-upload/basic/step-2.php
+++ file-upload/basic/step-2.php
@@ -1,28 +1,21 @@
 <?php
 if(isset($_POST['btnsubmit'])){
-  $tmp_name=$_FILES["myfile"]["tmp_name"];
-  $name=$_FILES['myfile']['name'];
-  
-	  copy($tmp_name,"image/".$name);
-	  
+  $tmp_name=$_FILES["m"]["tmp_name"];
+  $name=$_FILES['m']['name'];
+	  copy($tmp_name,"image/".$name);  
 }
 ?>
-
-
 <body>
-<div style=" width:500px; margin:10px auto">
+<div style="width:500px; margin:10px auto">
 <form action="#" method="post" enctype="multipart/form-data">
 <fieldset>
-<input type="file" name="myfile"><br><br>
+<input type="file" name="m"><br><br>
 <input type="submit" name="btnsubmit"><br>
 </fieldset>
 </form>
 
-
-
 <?php
 if(isset($_POST['btnsubmit'])){
-
   echo "<img src='image/$name' width='300px'>";
 }
 
