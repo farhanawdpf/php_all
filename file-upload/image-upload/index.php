edccc39b57e99985a8c780ddb71abe763f8643e2
farhanawdpf <farhanawdpf@gmail.com>
2026-04-09 10:40:30 +0600

    upload

--- file-upload/image-upload/index.php
+++ file-upload/image-upload/index.php
@@ -1,56 +1,55 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
 <?php
+$folder = "img/"; // uploads folder
+
 if (isset($_POST['submitbtn'])) {
-    $fileName = $_FILES['f']['name'];
-    $tmp = $_FILES['f']['tmp_name'];
-    $size = $_FILES['f']['size'];
-    $typ = pathinfo($fileName, PATHINFO_EXTENSION);
-    $s = "img/";
-    $kb = $size / 1024;
-
-    if ($kb > 400) {
-        echo "File is too large";
-    } elseif ($typ == "jpg" || $typ == "png") {
-        move_uploaded_file("$tmp", "$s.$fileName");
-    } else {
-        echo "only jpg,png uploaded";
-    }
-
-    // if ($typ == "jpg" || $typ == "png") {
-    //     move_uploaded_file("$tmp", "$s.$fileName");
-    // } else {
-    //     echo "only jpg,png uploaded";
-    // }
-    // move_uploaded_file("from","to");
+    $file = $_FILES['f']['tmp_name'];
+    $name = $_FILES['f']['name'];
+    move_uploaded_file($file, $folder . $name);
 }
-
-
-
-
 ?>
 
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Simple Gallery</title>
+</head>
 
 <body>
-    <form action="" method="post" enctype="multipart/form-data">
-        <input type="file" name="f">
-        <input type="submit" name="submitbtn">
 
+    <h2>Upload Image</h2>
+    <form method="post" enctype="multipart/form-data">
+        <input type="file" name="f" required>
+        <input type="submit" name="submitbtn" value="Upload">
     </form>
+
+    <h2>Gallery</h2>
     <?php
-    if (isset($_POST['submitbtn'])) {
-        echo "<img src='$s.$fileName' alt='image' width='400px'>";
+    $files = scandir($folder);
+    foreach ($files as $file) {
+        if ($file != '.' && $file != '..') {
+            echo "<img src='" . $folder . $file . "' width='150' style='margin:5px'>";
+        }
     }
-
-
-
     ?>
+
+
+
+
+
+    <pre>
+
+    What is scandir()?
+
+    scandir() is a built-in PHP function that reads a folder and returns all files and folders inside it as an array.
+     . → current folder
+    .. → parent folder
+
+    scandir(string $directory, int $sorting_order = SCANDIR_SORT_ASCENDING)
+$directory → the folder you want to scan.
+$sorting_order → optional, ascending or descending order.
+Returns → an array of filenames, including . (current folder) and .. (parent folder).
+</pre>
 
 </body>
 
--- file-upload/upload-2/up.php
+++ file-upload/upload-2/up.php
@@ -1,28 +1,34 @@
 <?php
-if (isset($_POST["btnsubmit"])){ 
+if (isset($_POST["btnsubmit"])) {
   $filename = $_FILES['myfile']['name'];
   $tmpfile = $_FILES['myfile']['tmp_name'];
   $img = 'image/';
- 
-   if(!empty($filename)){ 
-    move_uploaded_file($tmpfile,$img.$filename);
-   } else { 
+
+  if (!empty($filename)) {
+    move_uploaded_file($tmpfile, $img . $filename);
+  } else {
     echo "please select a file";
-   }
+  }
 }
- 
+
 ?>
 
 <body>
-<form action="" method="post" enctype="multipart/form-data"> 
-  Image :
-  <input type="file" name="myfile" ><br><br>
-  <input type="submit" name="btnsubmit" value="Upload"> 
-</form>
+  <form action="" method="post" enctype="multipart/form-data">
+    Image :
+    <input type="file" name="myfile"><br><br>
+    <input type="submit" name="btnsubmit" value="Upload">
+  </form>
 
-<?php
-if(isset($_POST["btnsubmit"])){ 
-	echo "<image src='$img/$filename' width='300px'>";
-}
-?>
+  <?php
+  // if (isset($_POST["btnsubmit"])) {
+  //   echo "<image src='$img/$filename' width='300px'>";
+  // }
+
+
+  foreach ($filename as $file) {
+
+    echo '<img src="' . $img / $file . '" width="150" style="margin:5px;">';
+  }
+  ?>
 </body>
